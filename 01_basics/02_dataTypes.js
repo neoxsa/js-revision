f26095ab2148a8a197c21016bfb5419b5aec4a85
NeoXsa <neoxsa8@gmail.com>
2025-10-22 22:19:44 +0530

    third commit

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all js code as newer version of js
+
+// alert(1 + 2) // we are using nodejs not browser
+
+console.log(1
+    +
+    2) // code readability should be high
+
+console.log("Sum")
+
+let name = "Admin"
+let age = 20
+let isLoggedIn = false
+let state;
+
+/* Primitive Data Types :-
+- number => 2 to power 53
+- string => ""
+- boolean => true/false
+- null => standalone value
+- undefined => undefined data type
+- bigint => for big numbers
+- symbol => unique
+*/
+
+// object
+
+console.log(typeof null);
+console.log(typeof undefined); 
