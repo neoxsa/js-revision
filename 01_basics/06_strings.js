cfca2e90e9b12de7913a90e2dde2c82cc0be5cce
NeoXsa <neoxsa8@gmail.com>
2025-10-28 22:32:25 +0530

    Update strings

--- 01_basics/06_strings.js
+++ 01_basics/06_strings.js
@@ -1,4 +1,4 @@
-let name = "neoxsa "
+let name = "neoxsa"
 let repo = 3
 
 // console.log(name + repo + " value");  // Not recommended
@@ -9,21 +9,21 @@ let anotherName = new String("gnome-42")
 
 // console.log(anotherName[0]);
 // console.log(anotherName.__proto__);
-
+console.log(name.concat(repo));
+console.log(name.fixed());
 // console.log(anotherName.length);
 // console.log(anotherName.toUpperCase());
-// console.log(anotherName.charAt(2));
-// console.log(anotherName.trim());
-// console.log(anotherName.indexOf('o'));
+// console.log(anotherName.charAt(2)); // o
+// console.log(anotherName.indexOf('o')); // 2
 
-const newName = anotherName.substring(0, 3)
-console.log(newName);
+const newName = anotherName.substring(3, 4) // start as per index value , the end value always neglect (not count)
+console.log(newName); //m
 
-const changedName = anotherName.slice(-4, 9)
+const changedName = anotherName.slice(3, -1) // if confusion then always minus the neg value to the length value
 console.log(changedName);
 
 const newString = "   neoxsa   "
-// console.log(newString.trim());
+console.log(newString.trim());
 
 const url = "https://google.com/download%20python"
 
