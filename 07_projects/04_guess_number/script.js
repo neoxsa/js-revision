7cfb5b903b2adcd9c9229aef1263e217472acd86
NeoXsa <neoxsa8@gmail.com>
2025-11-14 23:42:36 +0530

    add guess num project

--- 07_projects/04_guess_number/script.js
+++ 07_projects/04_guess_number/script.js
@@ -1,13 +1,13 @@
 let randomNumber = parseInt(Math.random() * 100 + 1);
 
-const submitBtn = document.querySelector('#sbt')
-const guessInput = document.querySelector('#guessField')
-const guessSlot = document.querySelector('.guesses')
-const guessRemain = document.querySelector('.lastResult')
-const lowOrHi = document.querySelector('.lowOrHi')
-const startOver = document.querySelector('.resultParas')
+const submitBtn = document.querySelector('#sbt');
+const guessInput = document.querySelector('#guessField');
+const guessSlot = document.querySelector('.guesses');
+const guessRemain = document.querySelector('.lastResult');
+const lowOrHi = document.querySelector('.lowOrHi');
+const startOver = document.querySelector('.resultParas');
 
-const p = document.createElement('p')
+const p = document.createElement('p');
 
 let prevGuess = []
 let guessNum = 1;
@@ -17,7 +17,7 @@ let playGame = true
 if (playGame) {
     submitBtn.addEventListener('click', function (e) {
         e.preventDefault();
-        const guessVal = parseInt(guessInput.value)
+        const guessVal = parseInt(guessInput.value);
         guessValidate(guessVal);
     })
 }
@@ -32,9 +32,9 @@ function guessValidate(guessVal) {
     } else {
         prevGuess.push(guessVal);
 
-        if (prevGuess === 11) {
+        if (guessNum === 10) {
+            displayMsg(`<h4 class="gameOver" >Game Over. Random no. was ${randomNumber} </h4>`);
             displayGuess(guessVal);
-            displayMsg(`Game Over. Random no. was ${randomNumber}`);
             endGame()
         } else {
             displayGuess(guessVal)
@@ -45,13 +45,13 @@ function guessValidate(guessVal) {
 
 function guessCheck(guessVal) {
     if (guessVal === randomNumber) {
-        displayMsg(`You Guessed it Right`);
+        displayMsg(`<p class="message">You Guessed it Right</p>`);
         endGame();
     } else if (guessVal > randomNumber) {
-        displayMsg(`Guessed Value is too High`);
+        displayMsg(`<p class="message">Guessed Value is too High</p>`);
     }
     else if (guessVal < randomNumber) {
-        displayMsg(`Guessed Value is too Low`);
+        displayMsg(`<p class="message">Guessed Value is too Low</p>`);
     }
 }
 
@@ -61,7 +61,6 @@ function displayGuess(guessVal) {
     guessSlot.innerHTML += `${guessVal},  `  //push the guessVal
     guessNum++;
     guessRemain.innerHTML = `${11 - guessNum}`
-
 }
 
 function displayMsg(message) {
@@ -72,7 +71,7 @@ function endGame() {
     guessInput.value = ""
     guessInput.setAttribute('disabled', '');
     p.classList.add('button')
-    p.innerHTML = `<h2 id='newGame'>Start new Game<h2/>`
+    p.innerHTML = `<h4 id='newGame'>Start new Game<h4/>`
     startOver.appendChild(p);
     playGame = false;
     newGame()
@@ -88,8 +87,8 @@ function newGame() {
         guessRemain.innerHTML = `${11 - guessNum}`
         guessInput.removeAttribute('disabled')
         startOver.removeChild(p)
-        guessInput.value = ''
         playGame = true
-    })
+        displayMsg(``);
+    });
 }
 
